31f53cbbc2bd9571733146752afae80f8db6b945
Shikanto <antonello.anzini.comunicazioni@gmail.com>
2021-10-20 09:29:40 +0200

    miss click change bg

---
+++ main.js
@@ -0,0 +1,59 @@
+const containerShell = document.getElementsByClassName("shell")[0];
+const selectDifficulty = document.getElementById("select_difficulty");
+const btnStartGame = document.getElementById("btn_start_game");
+
+
+
+
+
+function numberCellsDifficulty(level){
+
+    let numberCells;
+
+    if (parseInt(level) === 3) {
+        numberCells = 49;
+    } else if (parseInt(level) === 2) {
+        numberCells = 81;
+    } else if (parseInt(level) === 1) {
+        numberCells = 100;
+    }
+
+    return numberCells;
+
+}
+
+
+
+btnStartGame.addEventListener("click", function() {
+
+    const level = selectDifficulty.value;
+
+    const cells = numberCellsDifficulty(level);
+
+    console.log(level);
+    console.log(cells);
+
+    buildMinefield(cells);
+
+});
+
+function buildMinefield (cells) {
+
+    const cellsDifficulty = Math.sqrt(cells);
+    const cellSize = 100 / cellsDifficulty;
+    
+    containerShell.innerHTML="";
+
+    for (let i=1; i <= cells; i++){
+
+        containerShell.innerHTML += `<div class="box d-flex justify-content-center align-items-center" style="width: ${cellSize}%; height:${cellSize}% ">
+                                        <div> ${i} </div>
+                                    </div>` ;
+        
+    
+
+    }
+   
+}
+
+
